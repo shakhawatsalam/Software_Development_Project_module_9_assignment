2ea4fee47c59219e4c99a5330c04611c5dbcbed5
Shakhawat Salam <shakhawatsalam726@gmail.com>
2025-01-24 19:23:42 +0600

    ready for deploy

--- event_management/settings.py
+++ event_management/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,15 +92,24 @@ WSGI_APPLICATION = 'event_management.wsgi.application'
 # }
 
 # # For Postgres
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'event_management',
+#         'USER': 'postgres',
+#         'PASSWORD': 'shawon',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'event_management',
-        'USER': 'postgres',
-        'PASSWORD': 'shawon',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://event_management_gjn9_user:HhDVkfZv2CZglkePO7tkFx4khM82fMtC@dpg-cu9p4raj1k6c73dvub30-a.oregon-postgres.render.com/event_management_gjn9',
+        conn_max_age=600
+    )
 }
 
 
