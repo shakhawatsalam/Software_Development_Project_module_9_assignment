fd1a341cd207f88acc7dbf9068639dee5d962e97
Shakhawat Salam <shakhawatsalam726@gmail.com>
2025-02-05 22:24:16 +0600

    [ready for deployment forent url chante]

--- event_management/settings.py
+++ event_management/settings.py
@@ -168,7 +168,8 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 
 
 
-FRONTEND_URL='http://127.0.0.1:8000'
+
+FRONTEND_URL='https://event-management-q5rr.onrender.com'
 
 
 LOGIN_URL='sign-in'
