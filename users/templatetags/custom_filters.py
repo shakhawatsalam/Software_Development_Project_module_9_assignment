d725467c36e1201cca57d2993bbe8353d3a55887
Shakhawat Salam <shakhawatsalam726@gmail.com>
2025-02-19 10:17:06 +0600

    [Fix in customfilters file]

--- users/templatetags/custom_filters.py
+++ users/templatetags/custom_filters.py
@@ -2,10 +2,7 @@ from django import template
 from datetime import datetime
 from django.utils import timezone
 
-
 register = template.Library()
-
-
 
 @register.filter
 def humanized_date(value):
@@ -13,10 +10,10 @@ def humanized_date(value):
         today = datetime.now().date()
         value = timezone.localtime(value)
         if value.date() == today:
-            return f"Today at {value.strftime("%I:%M %p")}"
-        elif value.date() == today.replace(day=today.day -1):
-            return f'Yesterday at {value.strftime("%I:%M %p")}'
-        else :
-            return f"{value.date().strftime('%B %d')}, {value.strftime("%I:%M %p")}"
+            return f"Today at {value.strftime('%I:%M %p')}"
+        elif value.date() == today.replace(day=today.day - 1):
+            return f"Yesterday at {value.strftime('%I:%M %p')}"
+        else:
+            return f"{value.date().strftime('%B %d')}, {value.strftime('%I:%M %p')}"
         
     return "No login record available"
